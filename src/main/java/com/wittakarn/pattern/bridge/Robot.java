3ba5455d797d2cb6fb8c36de7c61cff32b306fd6
Wittakarn <wittakarn.expert@gmail.com>
2014-08-07 22:50:25 +0700

    Create project

---
+++ src/main/java/com/wittakarn/pattern/bridge/Robot.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wittakarn.pattern.bridge;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Wittakarn
+ */
+public class Robot implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    
+}
